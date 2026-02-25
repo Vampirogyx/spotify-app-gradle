5e90cb69aec8f535a4bd664bb25d8dea64530b2b
GabrielVS <gabriel.vegsan@educa.jcyl.es>
2026-02-25 23:28:15 +0100

    Start proyect final version

---
+++ src/main/java/edu/iesam/features/authors/presentation/AuthorView.java
@@ -0,0 +1,26 @@
+package edu.iesam.features.authors.presentation;
+
+import edu.iesam.features.authors.data.AuthorDataRepository;
+import edu.iesam.features.authors.data.AuthorMemLocalDataSource;
+import edu.iesam.features.authors.domain.Author;
+import edu.iesam.features.authors.domain.GetAuthorsUseCase;
+
+
+import java.util.ArrayList;
+
+public class AuthorView {
+
+    public static void printAuthors() {
+        GetAuthorsUseCase getAuthorsUseCase = new GetAuthorsUseCase(
+                new AuthorDataRepository(
+                        new AuthorMemLocalDataSource()));
+
+        ArrayList<Author> authors = getAuthorsUseCase.execute();
+        System.out.println(authors);
+    }
+
+    public static void saveAuthors() {
+        Author newAuthor = new Author("2", "name2", "2000", "España");
+    }
+
+}
